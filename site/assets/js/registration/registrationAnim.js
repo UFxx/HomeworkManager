8a33a87bfb610bb971f67acae9106e741fc82c91
Dmitriy <87441804+UFxx@users.noreply.github.com>
2023-12-18 21:40:21 +0300

    Added fiels (registration)

--- site/assets/js/registration/registrationAnim.js
+++ site/assets/js/registration/registrationAnim.js
@@ -1,11 +1,21 @@
-const inputEmail = document.querySelector('#registration-input');
+const inputEmail = document.querySelector('#email-input');
+const usernameInput = document.querySelector('#username-input');
 const inputPassword = document.querySelector('#registration-password');
 const repeatInputPassword = document.querySelector('#repeat-registration-password');
+const telInput = document.querySelector('#tel-input');
+const groupInput = document.querySelector('#group-input');
+const firstName = document.querySelector('#firstname-input');
+const lastName = document.querySelector('#lastname-input');
 const button = document.querySelector('form button');
 
 window.addEventListener('load', () => {
     inputEmail.style.transform = 'scale(1)';
+    usernameInput.style.transform = 'scale(1)';
     inputPassword.style.transform = 'scale(1)';
     repeatInputPassword.style.transform = 'scale(1)';
+    telInput.style.transform = 'scale(1)';
+    groupInput.style.transform = 'scale(1)';
+    firstName.style.transform = 'scale(1)';
+    lastName.style.transform = 'scale(1)';
     button.style.transform = 'scale(1)';
 })
