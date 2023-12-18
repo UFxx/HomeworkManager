16ec47266c5c6ca6fc43f68ef5ca09f44b107b9b
Kusov Alexandr <kusov.alex2004@mail.ru>
2023-12-18 19:57:05 +0300

    added database

--- hackathon/electronic_journal/admin.py
+++ hackathon/electronic_journal/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import (Group, Subject, Quest, UserQuest, User)
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_login', 'username', 'email', 'phone', 'role')
+    list_display_links = ('id', 'last_login', 'username', 'email', 'phone')
+    search_fields = ('id', 'username', 'phone', 'email')
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'curs')
+    list_display_links = ('id', 'name', 'curs')
+    search_fields = ('id', 'name')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class QuestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'quest_name', 'subject', 'teacher', 'group', 'description', 'date_added', 'date_pass')
+    list_display_links = ('id', 'subject')
+    search_fields = ('id', 'subject', 'description')
+    prepopulated_fields = {'slug': ('quest_name',)}
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject_name', 'subject_code', 'description')
+    list_display_links = ('id', 'subject_name')
+    search_fields = ('id', 'subject_name')
+    prepopulated_fields = {'slug': ('subject_name',)}
+
+
+class UserQuestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'quest', 'user', 'comment', 'date_added')
+    list_display_links = ('id', 'user')
+    search_fields = ('id', 'quest')
+    # prepopulated_fields = {'slug': ('user', 'quest')}
+
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Quest, QuestAdmin)
+admin.site.register(UserQuest, UserQuestAdmin)
+admin.site.register(User, UserAdmin)
--- hackathon/electronic_journal/models.py
+++ hackathon/electronic_journal/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
         ('admin', 'admin'),
         ('student', 'student'),
     )
-    group = models.ForeignKey('Group', on_delete=models.PROTECT, verbose_name='Группа')
+    group = models.ForeignKey('Group', on_delete=models.PROTECT, verbose_name='Группа', blank=True, null=True)
     slug = AutoSlugField(populate_from='username', unique=True, db_index=True, verbose_name='URL', )
     phone = models.CharField(max_length=15, verbose_name='Телефон', blank=True, null=True)
     verification = models.BooleanField(default=False, verbose_name='Верификация')
@@ -59,7 +59,7 @@ class Quest(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name='Группа')
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
     file_link = models.FileField(upload_to='quest/%Y/%m/%d/', blank=True, null=True)
-    date_added = models.DateField(auto_now_add=True, blank=True, verbose_name='Дата Добавлена')
+    date_added = models.DateField(auto_now_add=True, blank=True, verbose_name='Дата Добавления')
     date_pass = models.DateField(blank=True, verbose_name='Дата сдачи')
     slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
 
@@ -75,14 +75,14 @@ class UserQuest(models.Model):
     status = models.BooleanField(default=True, verbose_name='Статус')
     quest = models.ForeignKey(Quest, on_delete=models.PROTECT, verbose_name='Задание')
     user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='Студент')
-    comment = models.TextField(verbose_name='Комментарий', blank=True, null=True )
+    comment = models.TextField(verbose_name='Комментарий', blank=True, null=True)
     file_link = models.FileField(upload_to='user_quest/%Y/%m/%d/', blank=True, null=True)
     date_added = models.DateField(auto_now_add=True, blank=True)
-    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', blank=True, null=True)
 
     class Meta:
-        verbose_name = 'Задание'
-        verbose_name_plural = 'Задания'
+        verbose_name = 'Готовая работа'
+        verbose_name_plural = 'Готовые работы'
 
     def __str__(self):
         return f"{self.user}-{self.quest}-{self.date_added}"
