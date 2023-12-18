7326cb10572297c875c759cb5d749fba97d31408
Kusov Alexandr <kusov.alex2004@mail.ru>
2023-12-18 21:36:02 +0300

    added UserRegistrationForm

--- hackathon/electronic_journal/forms.py
+++ hackathon/electronic_journal/forms.py
@@ -1,9 +1,27 @@
 from django import forms
-
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from .models import *
 
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': "Логин", 'id': "login-input"}))
-    password = forms.CharField(label="", widget=forms.PasswordInput(attrs={'placeholder': "Пароль", 'id': "login-password"}))
+    password = forms.CharField(label="",
+                               widget=forms.PasswordInput(attrs={'placeholder': "Пароль", 'id': "login-password"}))
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+    group = forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите группу'})
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'}))
+    phone = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите номер телефона'}))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'}))
--- hackathon/electronic_journal/views.py
+++ hackathon/electronic_journal/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+from .models import User
 
 
 # Create your views here.
@@ -13,4 +13,14 @@ class Login(LoginView):
     template_name = 'electronic_journal/login.html'
 
     def get_success_url(self):
-        return reverse_lazy('login') #указать профиль
+        return reverse_lazy('login')  # указать профиль
+
+
+class UserRegistrationView(CreateView):
+    """Регистрация"""
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
+    success_message = 'Вы успешно зарегистрированы'
+    title = 'Store - Регистрация'
