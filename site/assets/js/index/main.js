bb884753bcfc2349f454eace1bec6065c1ee3e9a
Dmitriy <87441804+UFxx@users.noreply.github.com>
2023-12-19 21:12:06 +0300

    Clear branch

--- site/assets/js/index/main.js
+++
@@ -1,9 +0,0 @@
-const inputEmail = document.querySelector('#login-input');
-const inputPassword = document.querySelector('#login-password');
-const button = document.querySelector('form button');
-
-window.addEventListener('load', () => {
-    inputEmail.style.transform = 'scale(1)';
-    inputPassword.style.transform = 'scale(1)';
-    button.style.transform = 'scale(1)';
-})
